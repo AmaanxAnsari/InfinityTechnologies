0d0e937448e422e60556d344f2a9823d62ebc4db
Amaan Ansari <amaan0076@gmail.com>
2024-02-21 08:37:11 +0530

    Deployment test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["localhost"],
     remotePatterns: [
@@ -12,4 +13,5 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+export default nextConfig;
