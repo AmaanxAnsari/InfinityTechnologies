085e63e70b219e2389e5bc04bdaebd1189ac7b8e
AditiMadavi <aditiyamadavi00@gmail.com>
2024-02-23 01:12:30 +0530

    revert

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -24,7 +24,8 @@ const Hero = () => {
                   <Link
                     href="/about"
                   >
-                    About                  </Link>
+                    About
+                  </Link>
                   {/* <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
